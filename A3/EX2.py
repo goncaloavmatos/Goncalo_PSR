7f254a6fbca5f642eb6898bf9ccfa627cfec66b5
goncaloavmatos <goncalomatos@ua.pt>
2021-10-28 16:40:51 +0100

    Remade Ex2 from scratch

--- A3/EX2.py
+++ A3/EX2.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 
 def addComplex(x, y):
-    a = x[0]
-    b = x[1]
-    c =y[0]
-    d=y[1]
-    return (a + c,b + d)
-
+    # Complex number addition: (a+b*i) + (c+d*i) = (a+c) + (b+d)*i
+    a= x[0]  #a é a parte real do primeiro numero
+    b= x[1]  #a é a parte imaginaria do primeiro numero
+    c= y[0] #c é a parte real do segundo numero
+    d= y[1] #d é a parte imaginaria do segundo numero
+    return (a+c, b+d) #valor real e imaginario da soma pela formula da soma de numeros complexos
 
 def multiplyComplex(x, y):
-    a = x[0]
-    b = x[1]
-    c = y[0]
-    d = y[1]
-    result_real= (a*c - b*d)
-    result_im = (a*d + b*c)
-    return(result_real, result_im)
+    # Complex number multiplication: (a+b*i) x (c+d*i) = (a*c-b*d) + (a*d+b*c)*i
+    a = x[0]  # a é a parte real do primeiro numero
+    b = x[1]  # a é a parte imaginaria do primeiro numero
+    c = y[0]  # c é a parte real do segundo numero
+    d = y[1]  # d é a parte imaginaria do segundo numero
+    return (a*c-b*d, a*d+b*c )  # valor real e imaginario da multiplicação pela formula da multiplicação de numeros complexos
 
-def printComplex(x, prefix=''):
-    # add code here ...
-    r= x[0]
-    i=x[1]
+def printComplex(x, prefix=''):  #prefixo vazio por defeito, mas pode ser editado
+        # add code here ...
+    r = x[0] #parte real é a primeira posição (0) do numero x
+    i = x[1] #parte imag é a segunda posição (1) do numero x
     print(prefix + str(r) + '+' + str(i) + 'i')
 
 def main():
@@ -34,13 +33,12 @@ def main():
     printComplex(c2, prefix='c2= ')
 
     # Test add
-    print('addition')
     c3 = addComplex(c1, c2)
-    printComplex(c3)
+    printComplex(c3,prefix='Addition: c3= ')
 
     # test multiply
-    print('multiplication')
-    printComplex(multiplyComplex(c1, c2))
+    c3 = multiplyComplex(c1, c2)
+    printComplex(c3,prefix='Multiplication: c3= ')
 
 if __name__ == '__main__':
     main()

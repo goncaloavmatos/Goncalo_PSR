181b76528372aac79fb001aa746d65537cbd5612
goncaloavmatos <goncalomatos@ua.pt>
2021-11-09 16:51:33 +0000

    Reorganiar pastas

--- A1/EX1/main.py
+++
@@ -1,9 +0,0 @@
-
-
-def main():
-	alunos=['martin','miguel', 'joel','gonçalo','bia']
-	for idx in range(0,len(alunos)):
-		print('Bem vindo a PSR, ' + str(alunos[idx]) + ', aluno nº ' + str(idx+1))
-	
-if __name__=="__main__":
-	main()

b9e17b464f88c2ea13ac8e5a4d062d4fba0cde2d
goncaloavmatos <goncalomatos@ua.pt>
2021-12-02 13:10:25 +0000

    1:22:00 of A6 P1

--- Parte4/EX2.py
+++ Parte4/EX2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+a#!/usr/bin/python3
 
 import cv2
 
---
+++ Parte8/psr_aula8_ex3/src/publisher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# license removed for brevity
+
+import rospy
+from std_msgs.msg import String
+import argparse
+
+def talker():
+
+    # .............................................................
+    # Initialization
+    # .............................................................
+
+    parser = argparse.ArgumentParser(description='Part8 example')
+    parser.add_argument('--message', type=str, default='Do not know what to say ', help='message to print')
+
+    args = vars(parser.parse_args())
+
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(1) # 10hz
+
+    # .............................................................
+    # Execution
+    # .............................................................
+
+    while not rospy.is_shutdown():
+        text_to_send = args['message'] + str(rospy.get_time())
+        rospy.loginfo(text_to_send)
+        pub.publish(text_to_send)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
